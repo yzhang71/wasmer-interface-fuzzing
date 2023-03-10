e36ffd7ac48bb667f51b49f69fe3a06205c9165f
yzhang71 <yzhang0701@gmail.com>
2023-03-09 19:28:27 -0500

    Add: raw input fuzz driver

---
+++ wasmer-2.3.0/fuzz/fuzz_targets/universal_cranelift_raw.rs
@@ -0,0 +1,32 @@
+#![no_main]
+
+use libfuzzer_sys::{arbitrary, arbitrary::Arbitrary, fuzz_target};
+use wasm_smith::{Config, ConfiguredModule};
+use wasmer::{imports, CompilerConfig, Instance, Module, Store};
+use wasmer_compiler_cranelift::Cranelift;
+use wasmer_engine_universal::Universal;
+
+fuzz_target!(|data: &[u8]| {
+    let wasm_bytes = data;
+
+    if let Ok(path) = std::env::var("DUMP_TESTCASE") {
+        use std::fs::File;
+        use std::io::Write;
+        let mut file = File::create(path).unwrap();
+        file.write_all(&wasm_bytes).unwrap();
+        return;
+    }
+
+    let mut compiler = Cranelift::default();
+    compiler.canonicalize_nans(true);
+    compiler.enable_verifier();
+    let engine = Universal::new(compiler).engine();
+    let store = Store::new(&engine);
+    let module = Module::new(&store, &wasm_bytes);
+    // match Instance::new(&module, &imports! {}) {
+    //     Ok(_) => {}
+    //     Err(e) => {
+    //         println!("{}", e);
+    //     }
+    // }
+});
