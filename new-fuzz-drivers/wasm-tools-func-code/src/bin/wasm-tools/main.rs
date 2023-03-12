1d29214c1e993e543cf583b3e71ff31abbaa0575
yzhang71 <yzhang0701@gmail.com>
2023-03-12 11:15:26 -0400

    Add: add combine of func-code fuzz drivers

---
+++ new-fuzz-drivers/wasm-tools-func-code/src/bin/wasm-tools/main.rs
@@ -0,0 +1,86 @@
+use anyhow::Result;
+use clap::Parser;
+use std::io;
+use std::process::ExitCode;
+
+macro_rules! subcommands {
+    ($(
+        $(#[$attr:meta])*
+        ($name:ident, $string:tt)
+    )*) => {
+        $(
+            #[cfg(feature = $string)]
+            mod $name;
+        )*
+
+        #[derive(Parser)]
+        #[clap(version = version())]
+        #[allow(non_camel_case_types)]
+        enum WasmTools {
+            $(
+                #[cfg(feature = $string)]
+                $(#[$attr])*
+                $name($name::Opts),
+            )*
+        }
+
+        impl WasmTools {
+            fn run(self) -> Result<()> {
+                match self {
+                    $(
+                        #[cfg(feature = $string)]
+                        Self::$name(opts) => opts.run(),
+                    )*
+                }
+            }
+        }
+    }
+}
+
+subcommands! {
+    (parse, "parse")
+    (validate, "validate")
+    (print, "print")
+    (smith, "smith")
+    (shrink, "shrink")
+    (mutate, "mutate")
+    (dump, "dump")
+    (objdump, "objdump")
+    (strip, "strip")
+    (compose, "compose")
+    (demangle, "demangle")
+    #[command(subcommand)]
+    (component, "component")
+    #[command(subcommand)]
+    (metadata, "metadata")
+}
+
+fn main() -> ExitCode {
+    let err = match <WasmTools as Parser>::parse().run() {
+        Ok(()) => return ExitCode::SUCCESS,
+        Err(e) => e,
+    };
+    // If an error happened and it's connected to something like `EPIPE` then
+    // don't print out an error and instead just silently exit with a failure.
+    // This prevents stray panic messages when the stdout pipe is closed, for
+    // example.
+    if let Some(io) = err.downcast_ref::<io::Error>() {
+        match io.kind() {
+            io::ErrorKind::BrokenPipe => return ExitCode::FAILURE,
+            _ => {}
+        }
+    }
+    eprintln!("Error: {:?}", err);
+    ExitCode::FAILURE
+}
+
+/// If CARGO_VERSION_INFO is set, use it, otherwise use CARGO_PKG_VERSION.
+fn version() -> &'static str {
+    option_env!("CARGO_VERSION_INFO").unwrap_or(env!("CARGO_PKG_VERSION"))
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    WasmTools::command().debug_assert()
+}
