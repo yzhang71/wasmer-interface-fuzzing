b6d1411d96d4e1d146b38a26632e7cbceedbc29f
yzhang71 <yzhang0701@gmail.com>
2023-04-21 13:21:18 -0400

    Add: fuzzing for deserialize function

--- deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-2cc16f02a4ee9a77.d
+++
@@ -1,5 +0,0 @@
-/home/dennis/Documents/Rust_Project/Fuzzing_Wasmer/wasmer-interface-fuzzing/deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-2cc16f02a4ee9a77.rmeta: src/main.rs
-
-/home/dennis/Documents/Rust_Project/Fuzzing_Wasmer/wasmer-interface-fuzzing/deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-2cc16f02a4ee9a77.d: src/main.rs
-
-src/main.rs:
--- deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-9545892ef1b4d8c2.d
+++
@@ -1,5 +0,0 @@
-/home/dennis/Documents/Rust_Project/Fuzzing_Wasmer/wasmer-interface-fuzzing/deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-9545892ef1b4d8c2.rmeta: src/main.rs
-
-/home/dennis/Documents/Rust_Project/Fuzzing_Wasmer/wasmer-interface-fuzzing/deserialize-buffer-fuzz/target/debug/deps/deserialize_buffer_fuzz-9545892ef1b4d8c2.d: src/main.rs
-
-src/main.rs:
